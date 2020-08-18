bed509390033a84a3c436bb44a8adf12079a8a64
feerglas <info@feerglas.com>
2020-08-18 17:19:17 +0200

    fix: add comments, uncommend non-zero exit code for travis

--- figma_extractor/figma_extractor.sh
+++ figma_extractor/figma_extractor.sh
@@ -7,7 +7,7 @@
 
 # set flags for execution of this file:
 # -e: make sure command returns non-zero exit code as soon as somehting fails
-# set -e
+set -e
 
 # During local development, we want to use the .env file. On CI, we
 # will pass these variables as options
@@ -16,8 +16,10 @@ then
   export $(cat .env | sed 's/#.*//g' | xargs)
 fi
 
+# Necessary variables
 ACCESS_TOKEN=${FIGMA_ACCESS_TOKEN}
 FILE_URL=${FIGMA_FILE_URL}
 JSON_FILENAME=${FIGMA_JSON_FILENAME}
 
+# Get JSON from Figma API
 curl -sH "X-Figma-Token: $FIGMA_ACCESS_TOKEN" "$FIGMA_FILE_URL" -o $FIGMA_JSON_FILENAME
