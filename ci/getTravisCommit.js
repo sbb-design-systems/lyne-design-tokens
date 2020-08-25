b7264734f1845ae48257cf1d862dfdead854299d
feerglas <info@feerglas.com>
2020-08-25 10:37:03 +0200

    chore: test job id

--- ci/getTravisCommit.js
+++ ci/getTravisCommit.js
@@ -19,6 +19,8 @@ module.exports = async (id) => {
       url: `https://api.travis-ci.org/build/${id}`
     };
 
+    console.log(requestConfig);
+
     const travisResponse = await axios.request(requestConfig);
 
     console.log('------------ response');
