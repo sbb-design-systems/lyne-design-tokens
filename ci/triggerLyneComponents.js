6a2c1284f61149969f009494280af6a2c61f7dd6
feerglas <info@feerglas.com>
2020-09-09 15:08:40 +0200

    chore: integrate new travis trigger helper

--- ci/triggerLyneComponents.js
+++ ci/triggerLyneComponents.js
@@ -2,45 +2,22 @@ const axios = require('axios');
 const shell = require('shelljs');
 const argv = require('minimist');
 const getCommit = require('./getTravisCommit');
+const triggerTravis = require('lyne-helper-trigger-travis');
 
 require('dotenv')
   .config();
-
-const triggerTravis = async (_commitMessage) => {
-  const travisUrl = 'https://api.travis-ci.org/repo/lyne-design-system%2Flyne-components/requests';
-  const travisToken = process.env.TRAVIS_TOKEN;
-  const commitMessage = `${_commitMessage} (triggered from design tokens build)`;
-  const requestHeaders = {
-    'Accept': 'application/json',
-    'Authorization': `token ${travisToken}`,
-    'Content-Type': 'application/json',
-    'Travis-API-Version': '3'
-  };
-
-  const body = {
-    request: {
-      message: commitMessage
-    }
-  }
-
-  console.log('-->> Will trigger lyne-components job with message: ', commitMessage);
-
-  const requestConfig = {
-    data: body,
-    headers: requestHeaders,
-    method: 'POST',
-    url: travisUrl
-  };
-
-  await axios.request(requestConfig);
-};
 
 (async () => {
   try {
     const jobId = argv(process.argv.slice(2))['i'];
     const commitMessage = await getCommit(jobId);
 
-    await triggerTravis(commitMessage);
+    await triggerTravis({
+      branchName: 'master',
+      message: commitMessage,
+      travisToken: process.env.TRAVIS_TOKEN,
+      travisUrl: 'https://api.travis-ci.org/repo/lyne-design-system%2Flyne-components/requests'
+    });
 
     shell.exit(0);
   } catch (e) {
