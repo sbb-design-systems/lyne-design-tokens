b2c9b105812d8d20576406f15518301aa3a37eb2
Yves Tscherry <info@feerglas.com>
2021-07-26 17:40:08 +0200

    feat: add colors for primary button

--- designTokens/button.js
+++ designTokens/button.js
@@ -1,14 +1,55 @@
 module.exports = {
-  color: {
-    default: {
-      background: {
-        value: '{core.color.red.regular.value}'
+  primary: {
+    color: {
+      active: {
+        background: {
+          value: '{core.color.red.150.value}'
+        },
+        border: {
+          value: '{core.color.red.150.value}'
+        },
+        text: {
+          value: '{core.color.black.14.value}'
+        }
+      },
+      default: {
+        background: {
+          value: '{core.color.red.regular.value}'
+        },
+        border: {
+          value: '{core.color.red.regular.value}'
+        },
+        text: {
+          value: '{core.color.white.value}'
+        }
+      },
+      disabled: {
+        background: {
+          value: '{core.color.black.15.value}'
+        },
+        border: {
+          value: '{core.color.black.15.value}'
+        },
+        text: {
+          value: '{core.color.black.11.value}'
+        }
+      },
+      hover: {
+        background: {
+          value: '{core.color.red.125.value}'
+        },
+        border: {
+          value: '{core.color.red.125.value}'
+        },
+        text: {
+          value: '{core.color.black.15.value}'
+        }
       }
-    }
-  },
-  size: {
-    padding: {
-      value: '{core.size.spacing.small.value}'
+    },
+    size: {
+      padding: {
+        value: '{core.size.spacing.small.value}'
+      }
     }
   }
 };
