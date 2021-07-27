826f61a0b631eea935aa8ac63f7873eb5a69ab79
Yves Tscherry <info@feerglas.com>
2021-07-27 15:51:24 +0200

    chore: remove color category attributes

--- designTokens/core/color.js
+++ designTokens/core/color.js
@@ -1,185 +1,98 @@
 module.exports = {
   black: {
     1: {
-      attributes: {
-        category: 'color'
-      },
       value: 'rgba(21,21,21,1)'
     },
     10: {
-      attributes: {
-        category: 'color'
-      },
       value: 'rgba(189,189,189,1)'
     },
     11: {
-      attributes: {
-        category: 'color'
-      },
       value: 'rgba(205,205,205,1)'
     },
     12: {
-      attributes: {
-        category: 'color'
-      },
       value: 'rgba(210,210,210,1)'
     },
     13: {
-      attributes: {
-        category: 'color'
-      },
       value: 'rgba(220,220,220,1)'
     },
     14: {
-      attributes: {
-        category: 'color'
-      },
       value: 'rgba(229,229,229,1)'
     },
     15: {
-      attributes: {
-        category: 'color'
-      },
       value: 'rgba(246,246,246,1)'
     },
     2: {
-      attributes: {
-        category: 'color'
-      },
       value: 'rgba(33,33,33,1)'
     },
     3: {
-      attributes: {
-        category: 'color'
-      },
       value: 'rgba(68,68,68,1)'
     },
     4: {
-      attributes: {
-        category: 'color'
-      },
       value: 'rgba(90,90,90,1)'
     },
     5: {
-      attributes: {
-        category: 'color'
-      },
       value: 'rgba(104,104,104,1)'
     },
     6: {
-      attributes: {
-        category: 'color'
-      },
       value: 'rgba(118,118,118,1)'
     },
     7: {
-      attributes: {
-        category: 'color'
-      },
       value: 'rgba(141,141,141,1)'
     },
     8: {
-      attributes: {
-        category: 'color'
-      },
       value: 'rgba(168,168,168,1)'
     },
     9: {
-      attributes: {
-        category: 'color'
-      },
       value: 'rgba(183,183,183,1)'
     },
     alpha: {
       10: {
-        attributes: {
-          category: 'color'
-        },
         value: 'rgba(0,0,0,.1)'
       },
       20: {
-        attributes: {
-          category: 'color'
-        },
         value: 'rgba(0,0,0,.2)'
       },
       50: {
-        attributes: {
-          category: 'color'
-        },
         value: 'rgba(0,0,0,.5)'
       },
       60: {
-        attributes: {
-          category: 'color'
-        },
         value: 'rgba(0,0,0,.6)'
       },
       70: {
-        attributes: {
-          category: 'color'
-        },
         value: 'rgba(0,0,0,.7)'
       }
     },
     regular: {
-      attributes: {
-        category: 'color'
-      },
       value: 'rgba(0,0,0,1)'
     }
   },
   red: {
     125: {
-      attributes: {
-        category: 'color'
-      },
       value: 'rgba(198,0,24,1)'
     },
     150: {
-      attributes: {
-        category: 'color'
-      },
       value: 'rgba(162,0,19,1)'
     },
     regular: {
-      attributes: {
-        category: 'color'
-      },
       value: 'rgba(235,0,0,1)'
     }
   },
   white: {
     alpha: {
       20: {
-        attributes: {
-          category: 'color'
-        },
         value: 'rgba(255,255,255,.5)'
       },
       50: {
-        attributes: {
-          category: 'color'
-        },
         value: 'rgba(255,255,255,.5)'
       },
       60: {
-        attributes: {
-          category: 'color'
-        },
         value: 'rgba(255,255,255,.6)'
       },
       70: {
-        attributes: {
-          category: 'color'
-        },
         value: 'rgba(255,255,255,.7)'
       }
     },
     regular: {
-      attributes: {
-        category: 'color'
-      },
       value: 'rgba(255,255,255,1)'
     }
   }
