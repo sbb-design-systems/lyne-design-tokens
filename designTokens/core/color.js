941db156c3287cadfa760ec2ef90dd9543806082
Yves Tscherry <info@feerglas.com>
2021-07-27 09:06:46 +0200

    feat: add core colors include color attribute

--- designTokens/core/color.js
+++ designTokens/core/color.js
@@ -1,66 +1,186 @@
 module.exports = {
   black: {
     1: {
-      value: '#151515'
+      attributes: {
+        category: 'color'
+      },
+      value: 'rgba(21,21,21,1)'
     },
     10: {
-      value: '#bdbdbd'
+      attributes: {
+        category: 'color'
+      },
+      value: 'rgba(189,189,189,1)'
     },
     11: {
-      value: '#cdcdcd'
+      attributes: {
+        category: 'color'
+      },
+      value: 'rgba(205,205,205,1)'
     },
     12: {
-      value: '#d2d2d2'
+      attributes: {
+        category: 'color'
+      },
+      value: 'rgba(210,210,210,1)'
     },
     13: {
-      value: '#dcdcdc'
+      attributes: {
+        category: 'color'
+      },
+      value: 'rgba(220,220,220,1)'
     },
     14: {
-      value: '#e5e5e5'
+      attributes: {
+        category: 'color'
+      },
+      value: 'rgba(229,229,229,1)'
     },
     15: {
-      value: '#f6f6f6'
+      attributes: {
+        category: 'color'
+      },
+      value: 'rgba(246,246,246,1)'
     },
     2: {
-      value: '#212121'
+      attributes: {
+        category: 'color'
+      },
+      value: 'rgba(33,33,33,1)'
     },
     3: {
-      value: '#444444'
+      attributes: {
+        category: 'color'
+      },
+      value: 'rgba(68,68,68,1)'
     },
     4: {
-      value: '#5a5a5a'
+      attributes: {
+        category: 'color'
+      },
+      value: 'rgba(90,90,90,1)'
     },
     5: {
-      value: '#686868'
+      attributes: {
+        category: 'color'
+      },
+      value: 'rgba(104,104,104,1)'
     },
     6: {
-      value: '#767676'
+      attributes: {
+        category: 'color'
+      },
+      value: 'rgba(118,118,118,1)'
     },
     7: {
-      value: '#8d8d8d'
+      attributes: {
+        category: 'color'
+      },
+      value: 'rgba(141,141,141,1)'
     },
     8: {
-      value: '#a8a8a8'
+      attributes: {
+        category: 'color'
+      },
+      value: 'rgba(168,168,168,1)'
     },
     9: {
-      value: '#b7b7b7'
+      attributes: {
+        category: 'color'
+      },
+      value: 'rgba(183,183,183,1)'
+    },
+    alpha: {
+      10: {
+        attributes: {
+          category: 'color'
+        },
+        value: 'rgba(0,0,0,.1)'
+      },
+      20: {
+        attributes: {
+          category: 'color'
+        },
+        value: 'rgba(0,0,0,.2)'
+      },
+      50: {
+        attributes: {
+          category: 'color'
+        },
+        value: 'rgba(0,0,0,.5)'
+      },
+      60: {
+        attributes: {
+          category: 'color'
+        },
+        value: 'rgba(0,0,0,.6)'
+      },
+      70: {
+        attributes: {
+          category: 'color'
+        },
+        value: 'rgba(0,0,0,.7)'
+      }
     },
     regular: {
-      value: '#000000'
+      attributes: {
+        category: 'color'
+      },
+      value: 'rgba(0,0,0,1)'
     }
   },
   red: {
     125: {
-      value: '#c60018'
+      attributes: {
+        category: 'color'
+      },
+      value: 'rgba(198,0,24,1)'
     },
     150: {
-      value: '#a20013'
+      attributes: {
+        category: 'color'
+      },
+      value: 'rgba(162,0,19,1)'
     },
     regular: {
-      value: '#eb0000'
+      attributes: {
+        category: 'color'
+      },
+      value: 'rgba(235,0,0,1)'
     }
   },
   white: {
-    value: '#ffffff'
+    alpha: {
+      20: {
+        attributes: {
+          category: 'color'
+        },
+        value: 'rgba(255,255,255,.5)'
+      },
+      50: {
+        attributes: {
+          category: 'color'
+        },
+        value: 'rgba(255,255,255,.5)'
+      },
+      60: {
+        attributes: {
+          category: 'color'
+        },
+        value: 'rgba(255,255,255,.6)'
+      },
+      70: {
+        attributes: {
+          category: 'color'
+        },
+        value: 'rgba(255,255,255,.7)'
+      }
+    },
+    regular: {
+      attributes: {
+        category: 'color'
+      },
+      value: 'rgba(255,255,255,1)'
+    }
   }
 };
