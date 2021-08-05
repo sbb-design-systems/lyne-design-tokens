97705a009a642c6243e1bdcc44959a33cca032dc
Yves Tscherry <info@feerglas.com>
2021-08-05 13:53:55 +0200

    feat: add group attribute to shadow tokens

--- designTokens/shadow.js
+++ designTokens/shadow.js
@@ -1,75 +1,92 @@
 /* eslint-disable sort-keys */
 
-const sizeCategory = {
-  attributes: {
-    implicitCategory: 'size'
-  }
-};
+const groupAttribute = (shadowLevel) => ({
+  group: shadowLevel
+});
 
-const shadowObject = (x1, y1, blur1, spread1, x2, y2, blur2, spread2, color1, color2, color3, color4) => ({
+const attributes = (shadowLevel) => ({
+  attributes: {
+    implicitCategory: 'size',
+    ...groupAttribute(shadowLevel)
+  }
+});
+
+const shadowObject = (shadowLevel, x1, y1, blur1, spread1, x2, y2, blur2, spread2, color1, color2, color3, color4) => ({
   shadow: {
     1: {
       offset: {
         x: {
           value: x1,
-          ...sizeCategory
+          ...attributes(shadowLevel)
         },
         y: {
           value: y1,
-          ...sizeCategory
+          ...attributes(shadowLevel)
         }
       },
       blur: {
         value: blur1,
-        ...sizeCategory
+        ...attributes(shadowLevel)
       },
       spread: {
         value: spread1,
-        ...sizeCategory
+        ...attributes(shadowLevel)
       }
     },
     2: {
       offset: {
         x: {
           value: x2,
-          ...sizeCategory
+          ...attributes(shadowLevel)
         },
         y: {
           value: y2,
-          ...sizeCategory
+          ...attributes(shadowLevel)
         }
       },
       blur: {
         value: blur2,
-        ...sizeCategory
+        ...attributes(shadowLevel)
       },
       spread: {
         value: spread2,
-        ...sizeCategory
+        ...attributes(shadowLevel)
       }
     }
   },
   soft: {
     1: {
       color: {
-        value: color1
+        value: color1,
+        attributes: {
+          ...groupAttribute(shadowLevel)
+        }
       }
     },
     2: {
       color: {
-        value: color2
+        value: color2,
+        attributes: {
+          ...groupAttribute(shadowLevel)
+        }
       }
     }
   },
   hard: {
     1: {
       color: {
-        value: color3
+        value: color3,
+        attributes: {
+          ...groupAttribute(shadowLevel)
+        }
       }
     },
     2: {
       color: {
-        value: color4
+        value: color4,
+        attributes: {
+          ...groupAttribute(shadowLevel)
+        }
       }
     }
   }
@@ -79,19 +96,19 @@ module.exports = {
   elevation: {
     level: {
       1: {
-        ...shadowObject(0, 1, 8, 0, 0, 1, 2, 0, '{color.smoke.alpha.5.value}', '{color.smoke.alpha.10.value}', '{color.smoke.alpha.10.value}', '{color.smoke.alpha.20.value}')
+        ...shadowObject(1, 0, 1, 8, 0, 0, 1, 2, 0, '{color.smoke.alpha.5.value}', '{color.smoke.alpha.10.value}', '{color.smoke.alpha.10.value}', '{color.smoke.alpha.20.value}')
       },
       3: {
-        ...shadowObject(0, 2, 16, 0, 0, 1, 4, 0, '{color.smoke.alpha.5.value}', '{color.smoke.alpha.10.value}', '{color.smoke.alpha.10.value}', '{color.smoke.alpha.20.value}')
+        ...shadowObject(3, 0, 2, 16, 0, 0, 1, 4, 0, '{color.smoke.alpha.5.value}', '{color.smoke.alpha.10.value}', '{color.smoke.alpha.10.value}', '{color.smoke.alpha.20.value}')
       },
       5: {
-        ...shadowObject(0, 4, 32, 0, 0, 1, 8, 0, '{color.smoke.alpha.5.value}', '{color.smoke.alpha.10.value}', '{color.smoke.alpha.10.value}', '{color.smoke.alpha.20.value}')
+        ...shadowObject(5, 0, 4, 32, 0, 0, 1, 8, 0, '{color.smoke.alpha.5.value}', '{color.smoke.alpha.10.value}', '{color.smoke.alpha.10.value}', '{color.smoke.alpha.20.value}')
       },
       7: {
-        ...shadowObject(0, 8, 64, 0, 0, 2, 16, 0, '{color.smoke.alpha.5.value}', '{color.smoke.alpha.10.value}', '{color.smoke.alpha.10.value}', '{color.smoke.alpha.20.value}')
+        ...shadowObject(7, 0, 8, 64, 0, 0, 2, 16, 0, '{color.smoke.alpha.5.value}', '{color.smoke.alpha.10.value}', '{color.smoke.alpha.10.value}', '{color.smoke.alpha.20.value}')
       },
       9: {
-        ...shadowObject(0, 16, 128, 0, 0, 4, 32, 0, '{color.smoke.alpha.5.value}', '{color.smoke.alpha.10.value}', '{color.smoke.alpha.10.value}', '{color.smoke.alpha.20.value}')
+        ...shadowObject(9, 0, 16, 128, 0, 0, 4, 32, 0, '{color.smoke.alpha.5.value}', '{color.smoke.alpha.10.value}', '{color.smoke.alpha.10.value}', '{color.smoke.alpha.20.value}')
       }
     }
   }
