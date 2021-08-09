b9f2d4278234435aac4c7ca2eb3fcb2cf3b499e6
Yves Tscherry <info@feerglas.com>
2021-08-09 12:56:26 +0200

    fix: remove px to rem transformer

--- build.js
+++ build.js
@@ -2,17 +2,6 @@ const StyleDictionary = require('style-dictionary')
   .extend(`${__dirname}/config.js`);
 const fs = require('fs');
 const _ = require('lodash');
-const typo = require('./designTokens/typo');
-const baseFontSize = typo.base.font.size.value;
-
-// Transform to calculate rem from pixel values based on basePxFontSize
-StyleDictionary.registerTransform({
-  matcher: (token) => token.attributes.implicitCategory === 'size',
-  name: 'size/pxToRem',
-  transformer: (token) => `${token.value / baseFontSize}rem`,
-  transitive: true,
-  type: 'value'
-});
 
 console.log('Build started...');
 console.log('\n==============================================');
