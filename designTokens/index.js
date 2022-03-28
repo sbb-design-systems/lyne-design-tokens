abbd2e6bab81607e6b46274dd4a312f3187f9516
Michael Zumstein <zumi@yokai.ch>
2022-03-28 15:02:07 +0200

    feat: layout design tokens
    - add grid columns and gutter tokens for the base grid variant for
    each break breakpoint
    - add page layout offset tokens for each break breakpoint

--- designTokens/index.js
+++ designTokens/index.js
@@ -3,6 +3,7 @@ module.exports = {
   border: require('./border'),
   breakpoint: require('./breakpoint'),
   color: require('./color'),
+  layout: require('./layout'),
   shadow: require('./shadow'),
   size: require('./size'),
   spacing: require('./spacing'),
