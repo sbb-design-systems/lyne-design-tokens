322569d25bebc6036791cbe5b2823f35b34694ff
Michael Zumstein <zumi@yokai.ch>
2022-03-28 17:44:55 +0200

    feat: rename token category names for documentation platform

--- designTokens/layout.js
+++ designTokens/layout.js
@@ -12,94 +12,94 @@ module.exports = {
       columns: {
         zero: {
           value: 4,
-          ...attributes('grid')
+          ...attributes('grid-base-columns')
         },
         micro: {
           value: 4,
-          ...attributes('grid')
+          ...attributes('grid-base-columns')
         },
         small: {
           value: 4,
-          ...attributes('grid')
+          ...attributes('grid-base-columns')
         },
         medium: {
           value: 8,
-          ...attributes('grid')
+          ...attributes('grid-base-columns')
         },
         large: {
           value: 8,
-          ...attributes('grid')
+          ...attributes('grid-base-columns')
         },
         wide: {
           value: 12,
-          ...attributes('grid')
+          ...attributes('grid-base-columns')
         },
         ultra: {
           value: 12,
-          ...attributes('grid')
+          ...attributes('grid-base-columns')
         }
       },
       gutter: {
         zero: {
           value: 16,
-          ...attributes('grid')
+          ...attributes('grid-base-gutter')
         },
         micro: {
           value: 16,
-          ...attributes('grid')
+          ...attributes('grid-base-gutter')
         },
         small: {
           value: 24,
-          ...attributes('grid')
+          ...attributes('grid-base-gutter')
         },
         medium: {
           value: 24,
-          ...attributes('grid')
+          ...attributes('grid-base-gutter')
         },
         large: {
           value: 32,
-          ...attributes('grid')
+          ...attributes('grid-base-gutter')
         },
         wide: {
           value: 32,
-          ...attributes('grid')
+          ...attributes('grid-base-gutter')
         },
         ultra: {
           value: 32,
-          ...attributes('grid')
+          ...attributes('grid-base-gutter')
         }
       }
-    },
-    offset: {
-      base: {
-        zero: {
-          value: 16,
-          ...attributes('layout')
-        },
-        micro: {
-          value: 20,
-          ...attributes('layout')
-        },
-        small: {
-          value: 48,
-          ...attributes('layout')
-        },
-        medium: {
-          value: 56,
-          ...attributes('layout')
-        },
-        large: {
-          value: 64,
-          ...attributes('layout')
-        },
-        wide: {
-          value: 72,
-          ...attributes('layout')
-        },
-        ultra: {
-          value: 124,
-          ...attributes('layout')
-        }
+    }
+  },
+  offset: {
+    base: {
+      zero: {
+        value: 16,
+        ...attributes('offset-base')
+      },
+      micro: {
+        value: 20,
+        ...attributes('offset-base')
+      },
+      small: {
+        value: 48,
+        ...attributes('offset-base')
+      },
+      medium: {
+        value: 56,
+        ...attributes('offset-base')
+      },
+      large: {
+        value: 64,
+        ...attributes('offset-base')
+      },
+      wide: {
+        value: 72,
+        ...attributes('offset-base')
+      },
+      ultra: {
+        value: 124,
+        ...attributes('offset-base')
       }
     }
   }
