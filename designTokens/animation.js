4c9d02e90aa64435508c2cc8cab6391cb5d3bc1f
Jeremias Peier <jeremias.peier@sbb.ch>
2022-05-05 09:49:11 +0200

    fix: remove doubled animation prefix

--- designTokens/animation.js
+++ designTokens/animation.js
@@ -3,30 +3,28 @@ const baseDuration = 40;
 const duration = (value) => `${value * Number(baseDuration)}ms`;
 
 module.exports = {
-  animation: {
-    duration: {
-      '-1x': {
-        value: duration(1)
-      },
-      '-2x': {
-        value: duration(2)
-      },
-      '-3x': {
-        value: duration(3)
-      },
-      '-4x': {
-        value: duration(4)
-      },
-      '-5x': {
-        value: duration(5)
-      },
-      '-6x': {
-        value: duration(6)
-      }
+  duration: {
+    '-1x': {
+      value: duration(1)
     },
-    easing: {
-      value: 'cubic-bezier(.47, .1, 1, .63)'
+    '-2x': {
+      value: duration(2)
+    },
+    '-3x': {
+      value: duration(3)
+    },
+    '-4x': {
+      value: duration(4)
+    },
+    '-5x': {
+      value: duration(5)
+    },
+    '-6x': {
+      value: duration(6)
     }
+  },
+  easing: {
+    value: 'cubic-bezier(.47, .1, 1, .63)'
   }
 };
 
