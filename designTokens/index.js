fbea86eda35288baf64ce08b5ef11254bdeeeb85
Jeri Peier <jeremias.peier@sbb.ch>
2022-05-25 08:37:34 +0200

    feat: focus outline design token (#405)

--- designTokens/index.js
+++ designTokens/index.js
@@ -3,6 +3,7 @@ module.exports = {
   border: require('./border'),
   breakpoint: require('./breakpoint'),
   color: require('./color'),
+  focus: require('./focus'),
   layout: require('./layout'),
   shadow: require('./shadow'),
   size: require('./size'),
