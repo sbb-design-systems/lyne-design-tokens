54731c38eeb51a7a887fcef42e174f4a09dcf479
Lukas Spirig <lukas.spirig@sbb.ch>
2022-06-27 11:15:35 +0200

    refactor: remove ms from js times

--- designTokens/animation.ts
+++ designTokens/animation.ts
@@ -1,27 +1,39 @@
-import { DesignTokens } from 'style-dictionary';
+import { DesignToken, DesignTokens } from 'style-dictionary';
 
 const baseDuration = 40;
-const duration = (value: number) => `${value * baseDuration}ms`;
+const duration = (value: number) => value * baseDuration;
+const attributes = () =>
+  <Partial<DesignToken>>{
+    attributes: {
+      category: 'time',
+    },
+  };
 
 export const animation: DesignTokens = {
   duration: {
     '-1x': {
       value: duration(1),
+      ...attributes(),
     },
     '-2x': {
       value: duration(2),
+      ...attributes(),
     },
     '-3x': {
       value: duration(3),
+      ...attributes(),
     },
     '-4x': {
       value: duration(4),
+      ...attributes(),
     },
     '-5x': {
       value: duration(5),
+      ...attributes(),
     },
     '-6x': {
       value: duration(6),
+      ...attributes(),
     },
   },
   easing: {
