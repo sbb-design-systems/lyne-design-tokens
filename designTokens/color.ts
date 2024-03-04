59a8e40518ea225c9487184131227c8c5c738aa9
Tommmaso Menga <tommaso.menga-94@hotmail.it>
2024-03-04 10:00:36 +0100

    style(color): remove '-default' from tokens

--- designTokens/color.ts
+++ designTokens/color.ts
@@ -2,269 +2,207 @@ import { DesignTokens } from 'style-dictionary';
 
 export const color: DesignTokens = {
   black: {
-    default: {
-      value: 'rgba(0,0,0,1)',
+    value: 'rgba(0,0,0,1)',
+  },
+  blackAlpha: {
+    0: {
+      value: 'rgba(0,0,0,0)',
     },
-    alpha: {
-      0: {
-        value: 'rgba(0,0,0,0)',
-      },
-      10: {
-        value: 'rgba(0,0,0,.1)',
-      },
-      15: {
-        value: 'rgba(0,0,0,.15)',
-      },
-      20: {
-        value: 'rgba(0,0,0,.2)',
-      },
-      30: {
-        value: 'rgba(0,0,0,.3)',
-      },
-      40: {
-        value: 'rgba(0,0,0,.4)',
-      },
-      50: {
-        value: 'rgba(0,0,0,.5)',
-      },
-      60: {
-        value: 'rgba(0,0,0,.6)',
-      },
-      70: {
-        value: 'rgba(0,0,0,.7)',
-      },
+    10: {
+      value: 'rgba(0,0,0,.1)',
+    },
+    15: {
+      value: 'rgba(0,0,0,.15)',
+    },
+    20: {
+      value: 'rgba(0,0,0,.2)',
+    },
+    30: {
+      value: 'rgba(0,0,0,.3)',
+    },
+    40: {
+      value: 'rgba(0,0,0,.4)',
+    },
+    50: {
+      value: 'rgba(0,0,0,.5)',
+    },
+    60: {
+      value: 'rgba(0,0,0,.6)',
+    },
+    70: {
+      value: 'rgba(0,0,0,.7)',
     },
   },
   midnight: {
-    default: {
-      value: 'rgba(21,21,21,1)',
-    },
+    value: 'rgba(21,21,21,1)',
   },
   charcoal: {
-    default: {
-      value: 'rgba(33,33,33,1)',
-    },
+    value: 'rgba(33,33,33,1)',
   },
   iron: {
-    default: {
-      value: 'rgba(68,68,68,1)',
-    },
+    value: 'rgba(68,68,68,1)',
   },
   anthracite: {
-    default: {
-      value: 'rgba(90,90,90,1)',
-    },
+    value: 'rgba(90,90,90,1)',
   },
   granite: {
-    default: {
-      value: 'rgba(104,104,104,1)',
-    },
+    value: 'rgba(104,104,104,1)',
   },
   metal: {
-    default: {
-      value: 'rgba(118,118,118,1)',
+    value: 'rgba(118,118,118,1)',
+  },
+  metalAlpha: {
+    0: {
+      value: 'rgba(118,118,118,0)',
     },
-    alpha: {
-      0: {
-        value: 'rgba(118,118,118,0)',
-      },
-      20: {
-        value: 'rgba(118,118,118,.2)',
-      },
+    20: {
+      value: 'rgba(118,118,118,.2)',
     },
   },
   smoke: {
-    default: {
-      value: 'rgba(141,141,141,1)',
+    value: 'rgba(141,141,141,1)',
+  },
+  smokeAlpha: {
+    0: {
+      value: 'rgba(141,141,141,0)',
     },
-    alpha: {
-      0: {
-        value: 'rgba(141,141,141,0)',
-      },
-      5: {
-        value: 'rgba(141,141,141,.05)',
-      },
-      10: {
-        value: 'rgba(141,141,141,.1)',
-      },
-      20: {
-        value: 'rgba(141,141,141,.2)',
-      },
+    5: {
+      value: 'rgba(141,141,141,.05)',
+    },
+    10: {
+      value: 'rgba(141,141,141,.1)',
+    },
+    20: {
+      value: 'rgba(141,141,141,.2)',
     },
   },
   storm: {
-    default: {
-      value: 'rgba(168,168,168,1)',
-    },
+    value: 'rgba(168,168,168,1)',
   },
   graphite: {
-    default: {
-      value: 'rgba(183,183,183,1)',
-    },
+    value: 'rgba(183,183,183,1)',
   },
   cement: {
-    default: {
-      value: 'rgba(189,189,189,1)',
+    value: 'rgba(189,189,189,1)',
+  },
+  cementAlpha: {
+    0: {
+      value: 'rgba(189,189,189,0)',
     },
-    alpha: {
-      0: {
-        value: 'rgba(189,189,189,0)',
-      },
-      20: {
-        value: 'rgba(189,189,189,.2)',
-      },
+    20: {
+      value: 'rgba(189,189,189,.2)',
     },
   },
   platinum: {
-    default: {
-      value: 'rgba(205,205,205,1)',
+    value: 'rgba(205,205,205,1)',
+  },
+  platinumAlpha: {
+    0: {
+      value: 'rgba(205,205,205,0)',
     },
-    alpha: {
-      0: {
-        value: 'rgba(205,205,205,0)',
-      },
-      20: {
-        value: 'rgba(205,205,205,.2)',
-      },
+    20: {
+      value: 'rgba(205,205,205,.2)',
     },
   },
   aluminium: {
-    default: {
-      value: 'rgba(210,210,210,1)',
-    },
+    value: 'rgba(210,210,210,1)',
   },
   silver: {
-    default: {
-      value: 'rgba(220,220,220,1)',
-    },
+    value: 'rgba(220,220,220,1)',
   },
   cloud: {
-    default: {
-      value: 'rgba(229,229,229,1)',
-    },
+    value: 'rgba(229,229,229,1)',
   },
   milk: {
-    default: {
-      value: 'rgba(246,246,246,1)',
-    },
+    value: 'rgba(246,246,246,1)',
   },
   red: {
-    default: {
-      value: 'rgba(235,0,0,1)',
+    value: 'rgba(235,0,0,1)',
+  },
+  redAlpha: {
+    0: {
+      value: 'rgba(235,0,0,0)',
     },
-    alpha: {
-      0: {
-        value: 'rgba(235,0,0,0)',
-      },
-      20: {
-        value: 'rgba(235,0,0,.2)',
-      },
-      90: {
-        value: 'rgba(235,0,0,.9)',
-      },
+    20: {
+      value: 'rgba(235,0,0,.2)',
     },
-    mode: {
-      dark: {
-        value: 'rgb(255,56,56,1)',
-      },
+    90: {
+      value: 'rgba(235,0,0,.9)',
+    },
+  },
+  redMode: {
+    dark: {
+      value: 'rgb(255,56,56,1)',
     },
   },
   red125: {
-    default: {
-      value: 'rgba(198,0,24,1)',
+    value: 'rgba(198,0,24,1)',
+  },
+  red125Alpha: {
+    0: {
+      value: 'rgba(198,0,24,0)',
     },
-    alpha: {
-      0: {
-        value: 'rgba(198,0,24,0)',
-      },
-      20: {
-        value: 'rgba(198,0,24,.2)',
-      },
+    20: {
+      value: 'rgba(198,0,24,.2)',
     },
   },
   red150: {
-    default: {
-      value: 'rgba(162,0,19,1)',
-    },
+    value: 'rgba(162,0,19,1)',
   },
   white: {
-    default: {
-      value: 'rgba(255,255,255,1)',
+    value: 'rgba(255,255,255,1)',
+  },
+  whiteAlpha: {
+    0: {
+      value: 'rgba(255,255,255,0)',
     },
-    alpha: {
-      0: {
-        value: 'rgba(255,255,255,0)',
-      },
-      20: {
-        value: 'rgba(255,255,255,.2)',
-      },
-      30: {
-        value: 'rgba(255,255,255,.3)',
-      },
-      50: {
-        value: 'rgba(255,255,255,.5)',
-      },
-      60: {
-        value: 'rgba(255,255,255,.6)',
-      },
-      70: {
-        value: 'rgba(255,255,255,.7)',
-      },
+    20: {
+      value: 'rgba(255,255,255,.2)',
+    },
+    30: {
+      value: 'rgba(255,255,255,.3)',
+    },
+    50: {
+      value: 'rgba(255,255,255,.5)',
+    },
+    60: {
+      value: 'rgba(255,255,255,.6)',
+    },
+    70: {
+      value: 'rgba(255,255,255,.7)',
     },
   },
   sky: {
-    default: {
-      value: 'rgba(0,121,199,1)',
-    },
+    value: 'rgba(0,121,199,1)',
   },
   night: {
-    default: {
-      value: 'rgba(20,58,133,1)',
-    },
+    value: 'rgba(20,58,133,1)',
   },
   violet: {
-    default: {
-      value: 'rgba(111,34,130,1)',
-    },
+    value: 'rgba(111,34,130,1)',
   },
   pink: {
-    default: {
-      value: 'rgba(207,64,130,1)',
-    },
+    value: 'rgba(207,64,130,1)',
   },
   autumn: {
-    default: {
-      value: 'rgba(232,78,16,1)',
-    },
+    value: 'rgba(232,78,16,1)',
   },
   orange: {
-    default: {
-      value: 'rgba(242,126,0,1)',
-    },
+    value: 'rgba(242,126,0,1)',
   },
   peach: {
-    default: {
-      value: 'rgba(252,187,0,1)',
-    },
+    value: 'rgba(252,187,0,1)',
   },
   lemon: {
-    default: {
-      value: 'rgba(255,222,21,1)',
-    },
+    value: 'rgba(255,222,21,1)',
   },
   brown: {
-    default: {
-      value: 'rgba(183,96,0,1)',
-    },
+    value: 'rgba(183,96,0,1)',
   },
   green: {
-    default: {
-      value: 'rgba(0,138,54,1)',
-    },
+    value: 'rgba(0,138,54,1)',
   },
   turquoise: {
-    default: {
-      value: 'rgba(0,165,155,1)',
-    },
+    value: 'rgba(0,165,155,1)',
   },
 };
