d10f1b2a4b189859008bb83a817b45f07a1018c3
Mirco <mirco.entw@gmail.com>
2024-03-14 11:35:30 +0100

    feat(tailwind-config): first test implementation of a tailwind-config generator

--- build.ts
+++ build.ts
@@ -1,12 +1,59 @@
 import { FormatterArguments } from 'style-dictionary/types/Format';
 import { config } from './config';
 import StyleDictionaryBase, { TransformedToken } from 'style-dictionary';
+import { makeSdTailwindConfig } from 'sd-tailwindcss-transformer';
 
-const StyleDictionary = StyleDictionaryBase.extend(config);
+const StyleDictionary = StyleDictionaryBase.extend(config).extend(
+  makeSdTailwindConfig({
+    tailwind: { content: ['./src/**/*.{ts,tsx,html}'] },
+    buildPath: 'dist/tailwind/',
+    type: 'all',
+    transforms: ['attribute/cti', 'name/cti/kebab', 'custom/tailwind/organizeprops'],
+    isVariables: true,
+    prefix: 'sbb',
+  }),
+);
 const fileHeader = StyleDictionary.formatHelpers.fileHeader;
 
 console.log('Build started...');
 console.log('\n==============================================');
+
+StyleDictionary.registerTransform({
+  type: 'attribute',
+  name: 'custom/tailwind/organizeprops',
+  transformer: (token) => {
+    const [rootCategory, secondCategory] = token.path;
+    if (rootCategory === 'color') {
+      token.path[0] = 'colors';
+    } else if (rootCategory === 'breakpoint') {
+      token.path[0] = 'screens';
+    } else if (rootCategory === 'animation' && secondCategory === 'duration') {
+      token.path.splice(0, 2, 'transitionDuration');
+    } else if (rootCategory === 'animation' && secondCategory === 'easing') {
+      token.path.splice(0, 2, 'transitionTimingFunction');
+    } else if (rootCategory === 'border' && secondCategory === 'width') {
+      token.path.splice(0, 2, 'borderWidth');
+    } else if (rootCategory === 'border' && secondCategory === 'radius') {
+      token.path.splice(0, 2, 'borderRadius');
+    } else if (rootCategory === 'focus' && secondCategory === 'outline') {
+      token.path.splice(0, 3, 'outlineOffset');
+    } else if (rootCategory === 'spacing' && secondCategory === 'responsive') {
+      // remove "responsive"
+      token.path.splice(1, 1);
+      // remove "xxxs" etc.
+      token.path.splice(2, 1);
+      // remove media query value at the end
+      token.name = token.name.split('-').slice(0, -1).join('-');
+    } else if (rootCategory === 'spacing' && secondCategory === 'fixed') {
+      token.path.splice(1, 1);
+    } else if (rootCategory === 'shadow' && secondCategory === 'elevation') {
+      // remove "level" and <level-value> (e.g. 3)
+      token.path.splice(2, 2, 'lvl' + token.path[3]);
+    }
+
+    return token;
+  },
+});
 
 StyleDictionary.registerFormat({
   formatter: ({ file, dictionary, options }: FormatterArguments) => {
