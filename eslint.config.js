b47582e1e480c62db76a28f8a69dc25e3393c4ef
Jeremias Peier <jeremias.peier@sbb.ch>
2024-04-09 10:46:41 +0200

    fix: config

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,19 @@
+// @ts-check
+
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
-import yaml from 'eslint-plugin-yaml';
+import eslintPluginYml from 'eslint-plugin-yml';
+import eslint from '@eslint/js';
 
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
-import pluginJs from '@eslint/js';
-
-// mimic CommonJS variables -- not needed if using CommonJS
-const filename = fileURLToPath(import.meta.url);
-const dirname = path.dirname(filename);
-const compat = new FlatCompat({
-  baseDirectory: dirname,
-  recommendedConfig: pluginJs.configs.recommended,
-});
-
+/** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigFile} */
 export default [
   {
     languageOptions: { globals: globals.es2020, ecmaVersion: 2020 },
-    plugins: { yaml },
-    ignores: ['!.github/*', 'dist/**/*'],
+    ignores: ['dist/**/*'],
   },
-  ...compat.extends('standard-with-typescript'),
+  eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  ...eslintPluginYml.configs['flat/standard'],
   eslintConfigPrettier,
 ];
